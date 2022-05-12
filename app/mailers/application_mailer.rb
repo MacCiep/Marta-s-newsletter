9036ff8bd575dd385721a674bfb1c5e3e92f2d7e
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-05-12 19:59:53 +0200

    Configure production and mailer configuration

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'martasopinska.kontakt@gmail.com'
   layout 'mailer'
 end
--- app/mailers/assign_to_newsletter_mailer.rb
+++ app/mailers/assign_to_newsletter_mailer.rb
@@ -1,6 +1,10 @@
 class AssignToNewsletterMailer < ApplicationMailer
   default from: 'martasopinska.kontakt@gmail.com'
 
+  def test_mail
+    mail(to: martasopinska.kontakt@gmail.com, subject: "Hi, I'm your first testing mail")
+  end
+
   def welcome_mail(user)
     @user = user
     mail(to: @user.email, subject: 'Hej, miło mi Cię powitać w moim newsletterze!')
