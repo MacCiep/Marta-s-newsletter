092c11c86a7b0563d31605eeb5116c2588133633
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-05-15 14:25:11 +0200

    Add validation for newsletter input

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
 
+  validates :name, :email, presence: true, length: { minimum: 1 }
+  validates :email, uniqueness: true
+  validates_format_of :email,
+                      :with => /\A(\S+)@(.+)\.(\S+)\z/
+
+
   scope :with_active, -> { where(active: true).order(last_send: :desc)}
 end
