26c3c60b7c7500d82b7d92c037383279218ea7d4
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-05-15 13:48:29 +0200

    Fix form_with path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
-  get "/newsletter", to: "users#new"
-  post "/newsletter", to: "users#create"
+  resources :newsletter, :controller=>"users", only: [:new, :create], path_names: {new: ''}
+  # get "/newsletter", to: "users#new"
+  # post "/newsletter", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
