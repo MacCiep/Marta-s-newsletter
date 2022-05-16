0a49525de0912e24ee5ff37a6264ad5b6a34bf79
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-05-16 14:27:37 +0200

    Change some page layout and add background Jobs with Redis and Resque

--- app/jobs/send_emails_job.rb
+++ app/jobs/send_emails_job.rb
@@ -5,7 +5,7 @@ class SendEmailsJob < ApplicationJob
     User.with_active.each do |user|
       days_difference = (Date.today - user.last_send).to_i
       if days_difference == 0
-        AssignToNewsletterMailer.test_mail().deliver()
+        AssignToNewsletterMailer.test_mail().deliver_now()
         user.version += 1
         user.last_send = Date.today
         user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+require 'resque/server'
+
 Rails.application.routes.draw do
   resources :newsletter, :controller=>"users", only: [:new, :create], path_names: {new: ''}
+
+  mount Resque::Server.new, at: '41413412556/jobs'
   # get "/newsletter", to: "users#new"
   # post "/newsletter", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
